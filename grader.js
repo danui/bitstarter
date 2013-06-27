cb57e8d2dda13bc97fe41338356b13db99ff8d5a
jin <hsienjin@gmail.com>
2013-06-27 10:19:56 +1000

    Upgrade grader.js to accept --url option.

--- grader.js
+++ grader.js
@@ -25,8 +25,33 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = 'checks.json';
+
+// http://stackoverflow.com/questions/11386492/accessing-line-number-in-v8-javascript-chrome-node-js
+Object.defineProperty(global, '__stack', {
+    get: function(){
+	var orig = Error.prepareStackTrace;
+	Error.prepareStackTrace = function(_, stack){ return stack; };
+	var err = new Error;
+	Error.captureStackTrace(err, arguments.callee);
+	var stack = err.stack;
+	Error.prepareStackTrace = orig;
+	return stack;
+    }
+});
+Object.defineProperty(global, '__line', {
+    get: function(){
+	return __stack[1].getLineNumber();
+    }
+});
+//http://stackoverflow.com/questions/13591785/does-node-js-have-anything-like-file-and-line-like-the-c-preprocessor
+Object.defineProperty(global, '__file', {
+    get: function(){
+	return __stack[1].getFileName().split('/').slice(-1)[0];
+    }
+});
 
 var assertFileExists = function (infile) {
     var instr = infile.toString();
@@ -45,8 +70,7 @@ var loadChecks = function (checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function (htmlfile, checksfile) {
-    var $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function ($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for (var i in checks) {
@@ -54,6 +78,11 @@ var checkHtmlFile = function (htmlfile, checksfile) {
 	out[checks[i]] = present;
     }
     return out;
+};
+
+var checkHtmlFile = function (htmlfile, checksfile) {
+    var $ = cheerioHtmlFile(htmlfile);
+    return checkHtml($, checksfile);
 };
 
 var clone = function (fn) {
@@ -72,11 +101,27 @@ if (require.main == module) {
 		, 'Path to index.html'
 		, clone(assertFileExists)
 		, HTMLFILE_DEFAULT)
+	.option('-u, --url <url>'
+		, 'URL of HTML file')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-}
-else {
+
+    // program.url takes precedence over program.file since it has no
+    // default.
+    if (program.url) {
+	restler.get(program.url).on('complete', function (response) {
+	    if (response instanceof Error) {
+		console.error('Invalid URL');
+		process.exit(1);
+	    }
+	    var checkJson = checkHtml(cheerio.load(response), program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	});
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+} else {
     exports.checkHtmlFile = checkHtmlFile;
 }
